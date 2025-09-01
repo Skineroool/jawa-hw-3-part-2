035c12b6b7a1778ba7a1324cf0586bb78d7ff11f
Skineroool <byfender45@gmail.com>
2025-09-01 10:55:09 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double height = 1.87 ;
+        double weight = 98;
+        double index = service.calculate(height, weight);
+        int indexBmi = (int) index;
+        System.out.println(indexBmi);
+        }
+    }
